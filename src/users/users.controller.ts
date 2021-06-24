d3fedb45442c4c566a57f0512c69c4edb8dde069
JongyunHa <popawaw@naver.com>
2021-06-24 09:16:38 +0900

    Body, Query, Param

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { JoinRequestDto } from './dto/join.request.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller('api/users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
   @Get() // Get /users/
   getUsers() {}
 
   @Post() // Post /users/
-  postUsers() {}
+  postUsers(@Body() data: JoinRequestDto) {}
 
   @Post('login') // Post /users/login/
   login() {}
