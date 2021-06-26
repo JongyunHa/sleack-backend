311888bc7dc1b9d6431d5d1f98b24f30c1244d9c
JongyunHa <gkwhddbs77@knou.ac.kr>
2021-06-26 18:44:47 +0900

    Exception Filter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './httpException.filter';
 
 // nsetjs hot reloading setup
 declare const module: any;
@@ -18,6 +19,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT || 3000;
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(port);
   console.log(`listening to port: ${port}`);
 
