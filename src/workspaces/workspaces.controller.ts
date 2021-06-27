da2f663e8460a771785f39fb0d5c1279ba3f0de7
JongyunHa <gkwhddbs77@knou.ac.kr>
2021-06-27 20:16:38 +0900

    ParseIntPipe, ParseArrayPipe, PickType

--- src/workspaces/workspaces.controller.ts
+++ src/workspaces/workspaces.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Delete, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { User } from 'src/common/decorators/user.decorator';
+import { Users } from 'src/entities/Users';
+import { CreateWorkspaceDto } from './dto/create-workpsace.dto';
+import { WorkspacesService } from './workspaces.service';
 
 @ApiTags('WORKSPACE')
 @Controller('api/workspaces/:url')
 export class WorkspacesController {
+  constructor(private workspacesService: WorkspacesService) {}
   @Get()
-  getmyWorkspaces() {}
+  async getmyWorkspaces(@User() user: Users) {
+    return await this.workspacesService.findMyWorkspaces(user.id);
+  }
 
   @Post()
-  createWorkspace() {}
+  createWorkspace(@User() user: Users, @Body() body: CreateWorkspaceDto) {}
 
   @Get(':url/members')
   getAllMembersFromWorkspace() {}
