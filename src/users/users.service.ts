840bfafcee1f3d8c4802c8b564f75a29e5f5dfac
JongyunHa <gkwhddbs77@knou.ac.kr>
2021-06-27 19:41:28 +0900

    typeorm transaction

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,25 +3,44 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from 'src/entities/Users';
 import { Repository } from 'typeorm';
 import bcrypt from 'bcrypt';
+import { WorkspaceMembers } from 'src/entities/WorkspaceMembers';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users)
     private usersRepository: Repository<Users>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMembersRepository: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepository: Repository<ChannelMembers>,
   ) {}
 
-  async join(email: string, nickname: string, password: string): Promise<void> {
+  async join(
+    email: string,
+    nickname: string,
+    password: string,
+  ): Promise<boolean> {
     const user = await this.usersRepository.findOne({ where: { email } });
     if (user) {
       throw new UnauthorizedException('이미 존재하는 사용자입니다.');
     }
     const hashedPassword = await bcrypt.hash(password, 12);
-    console.log(hashedPassword);
-    await this.usersRepository.save({
+
+    const returned = await this.usersRepository.save({
       email,
       password: hashedPassword,
       nickname,
     });
+    await this.workspaceMembersRepository.save({
+      UserId: returned.id,
+      WorkspaceId: 1,
+    });
+    await this.channelMembersRepository.save({
+      UserId: returned.id,
+      ChannelId: 1,
+    });
+    return true;
   }
 }
